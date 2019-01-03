2b769550a8d3bfb6e9ab5a4453996fda3999877b
jfan3 <jfan3@wellesley.edu>
2019-01-03 17:42:25 -0600

    changes

--- predict/api.py
+++ predict/api.py
@@ -1,4 +1,4 @@
-from flask import *
+# from flask import *
 # import findspark
 # findspark.init()
 import pyspark
@@ -6,22 +6,34 @@ from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline,PipelineModel
 from pyspark.ml.classification import LogisticRegression, LogisticRegressionModel
 import requests
+from flask import Flask, render_template,request,jsonify
+
 
 app = Flask(__name__)
 
-@app.route('/predict',methods = ['POST', 'GET'])
+@app.route('/',methods = ['POST', 'GET'])
 def predict ():
    model = PipelineModel.load("models/pipeline_model")
-   response = requests.get('http://web-interface:5000')
-   data_dict = response.json().to_dict()
-   data_temp = [int(i[1]) for i in data_dict.items()]
-   data = spark.createDataFrame([data_temp], ["BENE_HMO_CVRAGE_TOT_MONS", "SP_CNCR", "TOTAL_DRUG_EXP"])
+   # response = requests.get('http://web-interface:5000')
+   # data_dict = response.json().to_dict()
+   data = request.get_json()['data']
+   # data_temp = [int(i[1]) for i in data_dict.items()]
+   data = spark.createDataFrame([list(data.values())], ["BENE_HMO_CVRAGE_TOT_MONS", "SP_CNCR", "TOTAL_DRUG_EXP"])
    result = model.transform(data).select("prediction").show()
-   return result
+   return jsonify({"prediction": result})
+
+# @app.route('/', methods=['POST'])
+# def predict():
+#     data = request.get_json()['data']
+#     feature = Vectors.dense(list(data.values()))
+#     test_data = spark.createDataFrame([{"features": feature}])
+#     predicted = model.transform(test_data)
+#     val = predicted.collect()[0]["prediction"]
+#     return jsonify({"prediction": val})
 
 if __name__ == '__main__':
    spark = SparkSession.builder.getOrCreate()
-   print('HereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHere')
+   # print('HereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHere')
 
    app.run(debug=True, host='0.0.0.0', port='5001')
 
--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request
 import requests
+import json
 app = Flask(__name__)
 
 
@@ -14,14 +15,20 @@ def form():
 #       # print([int(i[1]) for i in result.to_dict().items()])
 #
 #       return render_template("result.html",result = result)
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/',methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
         data = request.form
-        result = requests.post('http://web-interface:5001/result', json=data.to_dict())
-        data['result'] = result
-        print(result)
-    return render_template("result.html", result=data)
+        x1=request.form['BENE_HMO_CVRAGE_TOT_MONS']
+        x2 = request.form['SP_CNCR']
+        x3 = request.form['TOTAL_DRUG_EXP']
+        url = 'http://web-interface:5001'
+        features = {'data': {'x1': int(x1), 'x2': int(x2),'x3': int(x3)}}
+        headers = {'content-type': 'application/json'}
+        prediction = requests.post(url, data=json.dumps(features), headers=headers)
+        data['result'] = prediction
+        print(prediction)
+    # return render_template("result.html", result=data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
